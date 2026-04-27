9685f003f603eb0cacaecd30255a6b4adeeca876
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-27 11:34:43 +0100

    Get class using Class.forName() with try-catch and ClassNotFoundException

---
+++ Num1.java
@@ -0,0 +1,16 @@
+public class Num1 {
+
+    // First of all, you need to get the class using Class.forName
+    // This is risky so you need a try-catch block
+
+    public static void main(String[] args){
+        try{
+            Class<?> warriorClass = Class.forName("Warrior");
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("Class does not exist.");
+        }
+
+    }
+    
+}
